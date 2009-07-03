8ab6238b5068520fe64108f1fb561801825e8d7c
maverick.woo <maverick.woo@d4f42608-6275-11de-9e79-e94433246604>
2009-07-03 00:03:13 +0000

    now the mime-type will be set all the times

--- trunk/buildall.bat
+++ trunk/buildall.bat
@@ -4,6 +4,8 @@ perl build.pl samples/GetEnv.java Java java 2> err.log
 perl build.pl samples/test.tex LaTeX tex 2> err.log
 perl build.pl samples/csvformat.pl Perl pl 2> err.log
 
+svn ps svn:mime-type text/html ..\html\*.html
+
 dir ..\html\*-c.html | tail -n 2 | head -n 1
 dir ..\html\*-html.html | tail -n 2 | head -n 1
 dir ..\html\*-java.html | tail -n 2 | head -n 1
